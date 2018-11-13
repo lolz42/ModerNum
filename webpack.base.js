0205bb337a678d1d1da551cda27982413732018b
Marko <markoafx@gmail.com>
2018-11-13 13:43:41 +0100

    'btn cursor'

--- webpack.base.js
+++ webpack.base.js
@@ -144,6 +144,7 @@ module.exports =  {
       new PurifyCSSPlugin({
           // Give paths to parse for rules. These should be absolute!
           paths: glob.sync(path.join(__dirname, 'src/*.html')),
+
         }),
       new webpack.HashedModuleIdsPlugin(),
 
